73fc8a0fe9daf7781280a4752e92d47b9f0190ab
HANCORE <m.fattah-ahmad@gmx.de>
2025-12-27 18:09:31 +0100

    Add files via upload

--- neovim.lua
+++ neovim.lua
@@ -10,20 +10,20 @@ return {
 				base00 = "#1F1F1F", -- Default background
 				base01 = "#929292", -- Lighter background (status bars)
 				base02 = "#1F1F1F", -- Selection background
-				base03 = "#f3eed9", -- Comments, invisibles
-				base04 = "#ffffff", -- Dark foreground
+				base03 = "#d5c9ac", -- Comments, invisibles
+				base04 = "#efebdc", -- Dark foreground
 				base05 = "#ffffff", -- Default foreground
-				base06 = "#ffffff", -- Light foreground
-				base07 = "#ffffff", -- Light background
+				base06 = "#efebdc", -- Light foreground
+				base07 = "#b6b6b6", -- Light background
 
 				-- Accent colors (base08-base0F)
 				base08 = "#F44336", -- Variables, errors, red
 				base09 = "#f44336", -- Integers, constants, orange
 				base0A = "#B78955", -- Classes, types, yellow
-				base0B = "#798897", -- Strings, green
+				base0B = "#8f4e35", -- Strings, green
 				base0C = "#e6d690", -- Support, regex, cyan
 				base0D = "#e75a50", -- Functions, keywords, blue
-				base0E = "#d5c9ac", -- Keywords, storage, magenta
+				base0E = "#77838a", -- Keywords, storage, magenta
 				base0F = "#a99b7a", -- Deprecated, brown/yellow
 			},
 		},
